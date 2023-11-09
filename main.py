0b3a013ff0117525dfcaea144c0c037c6ce5958a
Eduardo Calvo <calvouribe.eduardo@gmail.com>
2023-11-09 17:27:36 +0900

    added detection with sample picture

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 
 # https://github.com/timesler/facenet-pytorch
 
-import cv2
 from facenet_pytorch import MTCNN, InceptionResnetV1
-from PIL import Image
+from PIL import Image, ImageDraw
 
 # If required, create a face detection pipeline using MTCNN:
-mtcnn = MTCNN(image_size=64, margin=0)
+mtcnn = MTCNN(image_size=160, margin=0)
 
 # Create an inception resnet (in eval mode):
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
@@ -14,12 +13,26 @@ resnet = InceptionResnetV1(pretrained='vggface2').eval()
 
 img = Image.open('./images/test.jpg')
 
+# Convert the image to RGB if it's not already in that mode
+if img.mode != 'RGB':
+    img = img.convert('RGB')
+
 # Get cropped and prewhitened image tensor
-img_cropped = mtcnn(img, save_path='./images/test_result.jpg')
+boxes, _ = mtcnn.detect(img) #, save_path='./images/test_result.jpg'
 
-# Calculate embedding (unsqueeze to add batch dimension)
-img_embedding = resnet(img_cropped.unsqueeze(0))
+# Create a copy of the original image to draw the boxes on
+draw_img = img.copy()
+draw = ImageDraw.Draw(draw_img)
 
-# Or, if using for VGGFace2 classification
-resnet.classify = True
-img_probs = resnet(img_cropped.unsqueeze(0))
+# Define box color and thickness
+box_color = 'green'
+thickness = 1
+
+# Draw the boxes
+if boxes is not None:
+    for box in boxes:
+        draw.rectangle(box.tolist(), outline=box_color, width=thickness)
+
+# Save or display the image
+draw_img.save('./images/test_with_boxes.jpg')
+draw_img.show()
