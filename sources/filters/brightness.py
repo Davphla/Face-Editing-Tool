d47e587e59f5f519b3b65a7d335a9c6343e31bb6
david <david.gozlan@epitech.eu>
2023-11-28 16:22:33 +0100

    feat - added brightness

--- sources/filters/brightness.py
+++ sources/filters/brightness.py
@@ -1,16 +1,20 @@
 from PIL import Image, ImageEnhance
 
+
+
 def apply_brightness(image_path, brightness_factor):
-   # Open the image file
-   image = Image.open(image_path)
+    # Open the image file
+    image = Image.open(image_path)
 
-   # Create a brightness enhancer
-   enhancer = ImageEnhance.Brightness(image)
+    for x in range(0, image.width):
+        for y in range(0, image.height):
+            r, g, b = image.getpixel((x, y))
+            r = int(r * brightness_factor)
+            g = int(g * brightness_factor)
+            b = int(b * brightness_factor)
+            image.putpixel((x, y), (r, g, b))
 
-   # Enhance the brightness of the image
-   brightened_image = enhancer.enhance(brightness_factor)
+    # Save the brightened image
+    image.save(image_path)
 
-   # Save the brightened image
-   brightened_image.save(image_path)
-
-   return image_path
+    return image_path
