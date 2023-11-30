4fcff4247cf20fd9c5a97277f6bd3ec942e2317b
david <david.gozlan@epitech.eu>
2023-11-30 08:56:06 +0100

    fix - mosaic tiled issue

--- sources/filters/mosaic.py
+++ sources/filters/mosaic.py
@@ -10,8 +10,8 @@ def apply_tile_average(image, x, y, size):
     return image
 
 def apply_mosaic_section(image, x1, y1, x2, y2):
-    for x in range(x1, x2, TILE_SIZE):
-        for y in range(y1, y2, TILE_SIZE):
+    for x in range(x1, x2 + TILE_SIZE, TILE_SIZE):
+        for y in range(y1, y2 + TILE_SIZE, TILE_SIZE):
             apply_tile_average(image, x, y, TILE_SIZE)
     return image
 
