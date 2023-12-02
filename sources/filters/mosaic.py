42f19a4822936c6d9de4c02dbad49427dee61fc2
jihak <jooin2000@naver.com>
2023-12-02 17:05:22 +0900

    change config - tile_size

--- sources/filters/mosaic.py
+++ sources/filters/mosaic.py
@@ -1,6 +1,8 @@
 # Importing necessary library
 from PIL import Image
-from config import TILE_SIZE
+
+
+# from config import TILE_SIZE
 
 def apply_tile_average(image, x, y, size):
     tile = image.crop((x, y, x + size, y + size))
@@ -9,11 +11,14 @@ def apply_tile_average(image, x, y, size):
     image.paste(rgb, (x, y, x + size, y + size))
     return image
 
+
 def apply_mosaic_section(image, x1, y1, x2, y2):
-    for x in range(x1, x2 + TILE_SIZE, TILE_SIZE):
-        for y in range(y1, y2 + TILE_SIZE, TILE_SIZE):
-            apply_tile_average(image, x, y, TILE_SIZE)
+    tile_size = 20
+    for x in range(x1, x2 + tile_size, tile_size):
+        for y in range(y1, y2 + tile_size, tile_size):
+            apply_tile_average(image, x, y, tile_size)
     return image
+
 
 def apply_mosaic(image_path):
     image = Image.open(image_path)
